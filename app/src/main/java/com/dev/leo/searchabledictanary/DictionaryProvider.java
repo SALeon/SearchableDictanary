3bd47d43142ec060df54f39e70b23c73dc122925
SALeon <developleon@gmail.com>
2017-02-16 11:50:41 +0200

    Update DictionaryProvider.java

--- app/src/main/java/com/dev/leo/searchabledictanary/DictionaryProvider.java
+++ app/src/main/java/com/dev/leo/searchabledictanary/DictionaryProvider.java
@@ -9,16 +9,13 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
-/**
- * Provides access to the dictionary database.
- */
+
 public class DictionaryProvider extends ContentProvider {
     String TAG = "DictionaryProvider";
 
     public static String AUTHORITY = "com.example.android.searchabledict.DictionaryProvider";
     public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/dictionary");
 
-    // MIME types used for searching words or looking up a single definition
     public static final String WORDS_MIME_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE +
             "/vnd.example.android.searchabledict";
     public static final String DEFINITION_MIME_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE +
@@ -26,31 +23,23 @@ public class DictionaryProvider extends ContentProvider {
 
     private DictionaryDatabase mDictionary;
 
-    // UriMatcher stuff
     private static final int SEARCH_WORDS = 0;
     private static final int GET_WORD = 1;
     private static final int SEARCH_SUGGEST = 2;
     private static final int REFRESH_SHORTCUT = 3;
     private static final UriMatcher sURIMatcher = buildUriMatcher();
 
-    /**
-     * Builds up a UriMatcher for search suggestion and shortcut refresh queries.
-     */
+
     private static UriMatcher buildUriMatcher() {
         UriMatcher matcher =  new UriMatcher(UriMatcher.NO_MATCH);
-        // to get definitions...
+ 
         matcher.addURI(AUTHORITY, "dictionary", SEARCH_WORDS);
         matcher.addURI(AUTHORITY, "dictionary/#", GET_WORD);
-        // to get suggestions...
+ 
         matcher.addURI(AUTHORITY, SearchManager.SUGGEST_URI_PATH_QUERY, SEARCH_SUGGEST);
         matcher.addURI(AUTHORITY, SearchManager.SUGGEST_URI_PATH_QUERY + "/*", SEARCH_SUGGEST);
 
-        /* The following are unused in this implementation, but if we include
-         * {@link SearchManager#SUGGEST_COLUMN_SHORTCUT_ID} as a column in our suggestions table, we
-         * could expect to receive refresh queries when a shortcutted suggestion is displayed in
-         * Quick Search Box, in which case, the following Uris would be provided and we
-         * would return a cursor with a single item representing the refreshed suggestion data.
-         */
+ 
         matcher.addURI(AUTHORITY, SearchManager.SUGGEST_URI_PATH_SHORTCUT, REFRESH_SHORTCUT);
         matcher.addURI(AUTHORITY, SearchManager.SUGGEST_URI_PATH_SHORTCUT + "/*", REFRESH_SHORTCUT);
         return matcher;
@@ -62,18 +51,11 @@ public class DictionaryProvider extends ContentProvider {
         return true;
     }
 
-    /**
-     * Handles all the dictionary searches and suggestion queries from the Search Manager.
-     * When requesting a specific word, the uri alone is required.
-     * When searching all of the dictionary for matches, the selectionArgs argument must carry
-     * the search query as the first element.
-     * All other arguments are ignored.
-     */
+  
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
 
-        // Use the UriMatcher to see what kind of query we have and format the db query accordingly
         switch (sURIMatcher.match(uri)) {
             case SEARCH_SUGGEST:
                 if (selectionArgs == null) {
@@ -102,8 +84,7 @@ public class DictionaryProvider extends ContentProvider {
                 BaseColumns._ID,
                 DictionaryDatabase.KEY_WORD,
                 DictionaryDatabase.KEY_DEFINITION,
-       /* SearchManager.SUGGEST_COLUMN_SHORTCUT_ID,
-                        (only if you want to refresh shortcuts) */
+  
                 SearchManager.SUGGEST_COLUMN_INTENT_DATA_ID};
 
         return mDictionary.getWordMatches(query, columns);
@@ -129,13 +110,7 @@ public class DictionaryProvider extends ContentProvider {
     }
 
     private Cursor refreshShortcut(Uri uri) {
-      /* This won't be called with the current implementation, but if we include
-       * {@link SearchManager#SUGGEST_COLUMN_SHORTCUT_ID} as a column in our suggestions table, we
-       * could expect to receive refresh queries when a shortcutted suggestion is displayed in
-       * Quick Search Box. In which case, this method will query the table for the specific
-       * word, using the given item Uri and provide all the columns originally provided with the
-       * suggestion query.
-       */
+
         String rowId = uri.getLastPathSegment();
         String[] columns = new String[] {
                 BaseColumns._ID,
@@ -147,10 +122,7 @@ public class DictionaryProvider extends ContentProvider {
         return mDictionary.getWord(rowId, columns);
     }
 
-    /**
-     * This method is required in order to query the supported types.
-     * It's also useful in our own query() method to determine the type of Uri received.
-     */
+
     @Override
     public String getType(Uri uri) {
         switch (sURIMatcher.match(uri)) {
@@ -167,7 +139,7 @@ public class DictionaryProvider extends ContentProvider {
         }
     }
 
-    // Other required implementations...
+
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
